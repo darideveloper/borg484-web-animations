364af54bbd72d20040cdf92a4dfe88bbc0095901
darideveloper <darideveloper@gmail.com>
2022-12-19 17:39:06 -0600

    christmas-landing-page-donations: Incress spheres size and reduce spheres place

--- christmas-landing-page-donations/js/custom.js
+++ christmas-landing-page-donations/js/custom.js
@@ -4,83 +4,82 @@ animation_time = 15
 let spheres_wrapper = document.querySelector('.spheres')
 
 function sleep(s) {
-    return new Promise(resolve => setTimeout(resolve, s*1000));
+  return new Promise(resolve => setTimeout(resolve, s * 1000))
 }
 
-async function create_sphere (size, text, photo) {
-    // Create a new sphere, add to wrapper and animate it
-    
-    // Crteate a new sphere
-    let sphere = document.createElement('div')
-    sphere.classList.add('sphere')
-    sphere.classList.add(size)
-    let sphere_text = document.createElement('span')
-    sphere_text.innerHTML = text
-    let sphere_photo = document.createElement('img')
-    sphere_photo.src = `./imgs/sample-donors/${photo}`
-    sphere.appendChild(sphere_text)
-    sphere.appendChild(sphere_photo)
+async function create_sphere(size, text, photo) {
+  // Create a new sphere, add to wrapper and animate it
 
-    // Add transsition
-    sphere.style.transitionDuration = `${animation_time}s`
+  // Crteate a new sphere
+  let sphere = document.createElement('div')
+  sphere.classList.add('sphere')
+  sphere.classList.add(size)
+  let sphere_text = document.createElement('span')
+  sphere_text.innerHTML = text
+  let sphere_photo = document.createElement('img')
+  sphere_photo.src = `./imgs/sample-donors/${photo}`
+  sphere.appendChild(sphere_text)
+  sphere.appendChild(sphere_photo)
 
-    // Add sphere to wrapper
-    spheres_wrapper.appendChild(sphere)
+  // Add transsition
+  sphere.style.transitionDuration = `${animation_time}s`
 
-    // Wait time after creation, based in size
-    if (size == "small") {
-        await sleep (animation_time/5)
-    } else if (size == "medium") {
-        await sleep (animation_time/4)
-    } else if (size == "large") {
-        await sleep (animation_time/3.5)
-    }
+  // Add sphere to wrapper
+  spheres_wrapper.appendChild(sphere)
 
-    // Animate sphere
-    sphere.style.transform = 'translateY(-120vh)'
+  // Wait time after creation, based in size
+  if (size == "small") {
+    await sleep(animation_time / 5)
+  } else if (size == "medium") {
+    await sleep(animation_time / 4)
+  } else if (size == "large") {
+    await sleep(animation_time / 3.5)
+  }
 
-    // Calculate and set random margin
-    let margin = Math.floor(Math.random() * (90)) - 10
-    sphere.style.marginLeft = `${margin}vw`
+  // Animate sphere
+  sphere.style.transform = 'translateY(-120vh)'
 
+  // Calculate and set random margin
+  const spheres_area = 70
+  let margin = Math.random() * (80 - 10 + 1)
+  sphere.style.marginLeft = `${margin}vw`
+}
+
+async function create_spheres() {
+  // Add new spheres with data from api
+
+  // Infinity loop
+  while (true) {
+
+    // Query from api
+    let { size, donor, photo } = get_donation()
+
+    // Create sphere
+    await create_sphere(size, donor, photo)
+  }
 
 }
 
-async function create_spheres () {
-    // Add new spheres with data from api
+async function remove_spheres() {
+  // Delete the firsts spheres after the animation
 
-    // Infinity loop
-    while (true) {
+  // Wait for first sphere
+  await sleep(animation_time + 2)
 
-        // Query from api
-        let {size, donor, photo} = get_donation ()
-    
-        // Create sphere
-        await create_sphere (size, donor, photo)
-    }
-
-}
-
-async function remove_spheres () {
-    // Delete the firsts spheres after the animation
-    
-    // Wait for first sphere
-    await sleep (animation_time + 2)
-
-    while (true) {
-        let last_sphere = spheres_wrapper.querySelector (".sphere:first-child")
-        spheres_wrapper.removeChild(last_sphere)
-        await sleep (animation_time)
-    }
+  while (true) {
+    let last_sphere = spheres_wrapper.querySelector(".sphere:first-child")
+    spheres_wrapper.removeChild(last_sphere)
+    await sleep(animation_time)
+  }
 
 }
 
 
 // Auto create and remove spheres
-create_spheres () 
-remove_spheres ()
+create_spheres()
+remove_spheres()
 
 // Refresh screen on resize
 window.addEventListener('resize', () => {
-    location.reload()
-});
+  location.reload()
+})
