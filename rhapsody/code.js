636195edf8e9efef028ef10d0f20c213cafb41d2
darideveloper <darideveloper@gmail.com>
2023-03-28 10:14:28 -0600

    feat (api) render donors from api.js

--- rhapsody/code.js
+++ rhapsody/code.js
@@ -1,18 +1,44 @@
-const selector_wrapper = '.thanks .donations'
-const wrapper = document.querySelector(selector_wrapper)
-const wrapper_height = wrapper.offsetHeight
-const selector_content = `${selector_wrapper} .content`
-const content_elem = document.querySelector(selector_content)
-const content_height = content_elem.offsetHeight
-const content_donations = document.querySelectorAll(`${selector_content} .donation`)
-const content_donations_count = content_donations.length
+async function init () {
 
-// Animate donors
-anime({
-  targets: selector_content,
-  translateY:-(content_height - wrapper_height + 100),
-  duration: 800 * content_donations_count,
-  loop: true,
-  direction: 'alternate',
-  easing: 'easeInOutQuad'
-});
+  // Get notes
+  const selector_wrapper = '.thanks .donations'
+  const selector_content = `${selector_wrapper} .content`
+  const wrapper_elem = document.querySelector(selector_wrapper)
+  const content_elem = document.querySelector(selector_content)
+  let content_donations = document.querySelectorAll(`${selector_content} .donation`)
+  
+  // Render donations
+  content_elem.innerHTML = "" // Clear wrapper
+  for (const donnor_name in donors) {
+    
+    // Create donation element
+    const donor_amount = donors[donnor_name]
+    const donation_elem = `<p class="donation">
+      <span class="name">${donnor_name}</span>
+      <span class="amount">${donor_amount}</span>
+    </p>`
+  
+    // Add donation element to wrapper
+    content_elem.innerHTML += donation_elem
+  }
+  
+
+  // Animate donations after render
+  content_donations = document.querySelectorAll(`${selector_content} .donation`)
+  const wrapper_height = wrapper_elem.offsetHeight
+  const content_height = content_elem.offsetHeight
+  const content_donations_count = content_donations.length
+  
+  // Animate donors
+  anime({
+    targets: selector_content,
+    translateY:-(content_height - wrapper_height + 100),
+    duration: 800 * content_donations_count,
+    loop: true,
+    direction: 'alternate',
+    easing: 'easeInOutQuad'
+  });
+
+}
+
+init ()
