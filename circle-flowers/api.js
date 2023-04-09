a24e19a1d918f42321ff6bd161048d98ca5b3323
darideveloper <darideveloper@gmail.com>
2023-04-08 18:23:36 -0600

    feat (circle-flowers) title in circle

--- circle-flowers/api.js
+++ circle-flowers/api.js
@@ -1,15 +1,10 @@
-export const donors = [
-  "John Doe",
-  "Jane Doe",
-  "John Smith",
-  "Jane Smith",
-  "John Black",
-  "Jane Black",
-  "John White",
-  "Jane White",
-  "John Brown",
-  "Jane Brown",
-  "John Green",
-  "Jane Green",
-]
-
+export const donors = {
+  "John Doe": 100,
+  "Jane Doe": 200,
+  "John Smith": 300,
+  "Jane Smith": 400,
+  "John Doe2": 500,
+  "Jane Doe2": 600,
+  "John Smith2": 700,
+  "Jane Smith2": 800,
+}
--- circle-flowers/code.js
+++ circle-flowers/code.js
@@ -1,17 +1,10 @@
 import { donors } from './api.js'
 
-const content = document.querySelector('.content-circle')
+const content_circle = document.querySelector('.content-circle')
+const bottom_flowers = document.querySelector('.bottom-flowers')
 
-// Render specific number of flowers in circle
-const num_flowers = 18
-const angule = 360 / num_flowers
-for (let num_flower = 0; num_flower < num_flowers; num_flower++) {
-
-  // Create flower
-  const current_angle = angule * num_flower
-  const current_flower = `
-  <div class="flower" style="transform: translate(-50%, -50%) rotate(${current_angle}deg);">
-    <div class="flower-container">
+const flower_content = `
+  <div class="flower-container">
       <div class="petals">
         <div class="petal-leaf"></div>
         <div class="petal-leaf"></div>
@@ -23,8 +16,37 @@ for (let num_flower = 0; num_flower < num_flowers; num_flower++) {
       </div>
     <div class="stem"></div>
   </div>  
+`
+
+// Render specific number of flowers in circle
+const num_flowers = 18
+const angule = 360 / num_flowers
+for (let num_flower = 0; num_flower < num_flowers; num_flower++) {
+
+  // Create flower
+  const current_angle = angule * num_flower
+  const current_flower = `
+  <div class="flower" style="transform: translate(-50%, -50%) rotate(${current_angle}deg);">
+    ${flower_content}
   </div>`
   
   // Append flower to body
-  content.innerHTML += current_flower
+  content_circle.innerHTML += current_flower
+}
+
+// Render flowers in bottom
+for (const donor in donors) {
+  const donor_name_lines = donor.split(' ')
+  const donor_name_elems = donor_name_lines.map(line => `<span>${line}</span>`)
+  const amount = donors[donor]
+  const current_donation = `
+    <div class="donation">
+      ${flower_content}
+      <p class="amount">$ ${amount}</p>
+      <p class="name">${donor_name_elems.join(" ")}</p>
+    </div>
+  `
+  
+  // Append flower to body
+  bottom_flowers.innerHTML += current_donation
 }
