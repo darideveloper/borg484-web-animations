389476633fbf2e06422de829ce9e287355f3cd26
darideveloper <darideveloper@gmail.com>
2023-04-10 00:57:59 -0600

    feat (circle-flowers) random angule

--- circle-flowers/code.js
+++ circle-flowers/code.js
@@ -26,7 +26,7 @@ const angule = 360 / num_flowers
 for (let num_flower = 0; num_flower < num_flowers; num_flower++) {
 
   // Create flower
-  const current_angle = angule * num_flower
+  const current_angle = Math.floor(Math.random() * angule ) + angule * num_flower
   const current_flower = `
   <div class="flower" style="transform: translate(-50%, -50%) rotate(${current_angle}deg);">
     ${flower_content}
