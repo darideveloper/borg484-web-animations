c62b03227f8e2518327328cbed6191bdd8c42cb1
darideveloper <darideveloper@gmail.com>
2023-04-14 23:37:07 -0600

    feat (superhero) data from api

--- superheros/code/api.js
+++ superheros/code/api.js
@@ -1,8 +1,27 @@
 function render_donations () {
+  // Place donations information from api
+
+  const hero_elems = document.querySelectorAll (".hero")
+
   fetch ("./code/api.json")
   .then (response => response.json())
   .then (data => {
-    console.log (data)
+    
+    data.forEach ((donation, index) => {
+      
+      // Get elements
+      const hero_elem = hero_elems [index]
+      const amount_elem = hero_elem.querySelector (".amount > .num")
+      const name1_elem = hero_elem.querySelector (".name1")
+      const name2_elem = hero_elem.querySelector (".name2")
+
+      // Place data
+      console.log (amount_elem)
+      amount_elem.innerHTML = donation.amount
+      name1_elem.innerHTML = donation.name1
+      name2_elem.innerHTML = donation.name2
+    })
+
   })
 }
 
