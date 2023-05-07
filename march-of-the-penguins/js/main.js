2d3b281218eb2809b1c2d0fc03d89b2f03aff619
darideveloper <darideveloper@gmail.com>
2023-05-07 11:06:30 -0600

    feat (march-of-the-penguins): render penguins with color variation

--- march-of-the-penguins/js/main.js
+++ march-of-the-penguins/js/main.js
@@ -1,6 +1,23 @@
-const donations = [{},{},{}]
+const donations = [{hello: "world"},{},{},{}]
 
 function init () {
   // Render penguins
-  
+
+  const penguins_container = document.querySelector ('.penguins')
+  donations.forEach (donation => {
+
+    // Calculate random hue rotation and brightness
+    const hue = Math.floor (Math.random () * 50)
+    
+    // Render new penguin
+    // const penguin_image = `<img src="./images/penguin.svg" alt="donation penguin" class="penguin">`
+    const penguin = document.createElement ('img')
+    penguin.src = './images/penguin.svg'
+    penguin.alt = 'donation penguin'
+    penguin.classList.add ('penguin')
+    penguin.style.filter = `hue-rotate(${hue}deg)`
+    penguins_container.appendChild (penguin)
+  })
 }
+
+init ()
