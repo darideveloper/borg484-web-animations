c78c027956dc686caa6087999d7ce7e327df7334
darideveloper <darideveloper@gmail.com>
2023-05-07 11:25:07 -0600

    feat (march-of-the-penguins): move penguins

--- march-of-the-penguins/js/main.js
+++ march-of-the-penguins/js/main.js
@@ -1,22 +1,51 @@
-const donations = [{hello: "world"},{},{},{}]
+const donations = [{}, {}, {}, {}, {}, {}, {}, {}, {}]
 
 function init () {
   // Render penguins
 
-  const penguins_container = document.querySelector ('.penguins')
+  const penguinsContainer = document.querySelector ('.penguins')
   donations.forEach (donation => {
 
-    // Calculate random hue rotation and brightness
-    const hue = Math.floor (Math.random () * 50)
+    // Calculate pinguin random options
+    const hue = Math.floor (Math.random () * 80)
+    const positionX = Math.random () > 0.5 ? 'left' : 'right'
+    const positionY = Math.floor (Math.random () * 20)
+    const size = Math.floor (Math.random () * 3) + 10
+    console.log ({hue, positionX, positionY, size}) 
     
     // Render new penguin
-    // const penguin_image = `<img src="./images/penguin.svg" alt="donation penguin" class="penguin">`
+
+    // html content
     const penguin = document.createElement ('img')
     penguin.src = './images/penguin.svg'
     penguin.alt = 'donation penguin'
     penguin.classList.add ('penguin')
+
+    // Styles
     penguin.style.filter = `hue-rotate(${hue}deg)`
-    penguins_container.appendChild (penguin)
+    if (positionX === 'left') {
+      penguin.style.left = '-13vw'
+    } else {
+      penguin.style.right = '-13vw'
+    } 
+    penguin.style.bottom = `${positionY}vh`
+    penguin.style.width = `${size}vw`
+
+    // Append to container
+    penguinsContainer.appendChild (penguin)
+
+    // Random animation time and delay
+    const animationTime = Math.floor (Math.random () * 10000) + 5000
+    const animationDelay = Math.floor (Math.random () * 10000)
+
+    // Animate penguin
+    anime({
+      targets: penguin,
+      translateX: positionX == 'left' ? '113vw' : '-113vw',
+      duration: animationTime,
+      delay: animationDelay,
+      easing: 'linear',
+    });
   })
 }
 
