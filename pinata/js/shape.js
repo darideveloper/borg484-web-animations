fa68703c2fc6a79c18c145a000181b9d99e156fd
daridev <darideveloper@gmail.com>
2023-09-07 23:33:51 -0600

    feat (pinata): random candy color

--- pinata/js/shape.js
+++ pinata/js/shape.js
@@ -1,5 +1,22 @@
 let Bodies = Matter.Bodies
 
+// Update candies images >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+const textureIImages = [
+  './imgs/candy1.png',
+  './imgs/candy2.png',
+  './imgs/candy3.png',
+  './imgs/candy4.png',
+  './imgs/candy5.png',
+  './imgs/candy6.png',
+  './imgs/candy7.png',
+  './imgs/candy8.png',
+  './imgs/candy9.png',
+  './imgs/candy10.png',
+]
+
+// Update candy circle size >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+const circleSize = 40
+
 function getShape () {
 
   var bodyOptions = {
@@ -10,12 +27,15 @@ function getShape () {
 
   let randomX = Math.floor(Math.random() * window.innerWidth)
   let randomY = Math.floor(Math.random() * window.innerHeight)
-  let randomRadius = Math.floor(40)
+  let randomRadius = Math.floor(circleSize)
 
   let shape = Bodies.circle(randomX, randomY, randomRadius, bodyOptions)
 
+  // Get a random texture image
+  let randomTexture = textureIImages[Math.floor(Math.random() * textureIImages.length)]
+
   // Add texture to shape
-  shape.render.sprite.texture = './imgs/candy.png' 
+  shape.render.sprite.texture = randomTexture
 
   return shape
 }
