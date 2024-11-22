500f5f9dc8a48dcc80870530c42255b9411a0779
daridev <darideveloper@gmail.com>
2024-11-22 10:06:01 -0600

    feat (dots-100): add companies logos

--- dots-100/code/dots.js
+++ dots-100/code/dots.js
@@ -96,7 +96,7 @@ class Dots {
     console.log({ createdDots, "requiredDots": this.requiredDots, dotsDiff, finalDots })
   }
 
-  addDonation(companyName) {
+  addDonation(companyName, imageSrc) {
 
     // Activate next dot
     const inactiveDots = document.querySelectorAll('.dot.over:not(.active)')
@@ -107,7 +107,12 @@ class Dots {
     }
 
     // Update marquee
-    this.marquee.innerHTML += `<span>${companyName}</span>`
+    this.marquee.innerHTML += `<div>
+      <div class="content">
+        <img src="${imageSrc}" alt="logo of ${companyName}" />
+        <span>${companyName}</span>
+      </div>
+    </div>`
   }
 }
 
@@ -121,81 +126,81 @@ dotsManager.draw100()
 
 // Add donations
 setTimeout(() => {
-  dotsManager.addDonation("Test Company A")
+  dotsManager.addDonation("Test Company A", "./imgs/logo.png")
 }, 1000)
 
 setTimeout(() => {
-  dotsManager.addDonation("Test Company B")
+  dotsManager.addDonation("Test Company B", "./imgs/logo.png")
 }, 2000)
 
 setTimeout(() => {
-  dotsManager.addDonation("Test Company C")
+  dotsManager.addDonation("Test Company C", "./imgs/logo.png")
 }, 3000)
 
 setTimeout(() => {
-  dotsManager.addDonation("Test Company D")
+  dotsManager.addDonation("Test Company D", "./imgs/logo.png")
 }, 5000)
 
 setTimeout(() => {
-  dotsManager.addDonation("Test Company E")
+  dotsManager.addDonation("Test Company E", "./imgs/logo.png")
 }, 8000)
 
 setTimeout(() => {
-  dotsManager.addDonation("Test Company F")
+  dotsManager.addDonation("Test Company F", "./imgs/logo.png")
 }, 8000)
 
 setTimeout(() => {
-  dotsManager.addDonation("Test Company G")
+  dotsManager.addDonation("Test Company G", "./imgs/logo.png")
 }, 9000)
 
 setTimeout(() => {
-  dotsManager.addDonation("Test Company H")
+  dotsManager.addDonation("Test Company H", "./imgs/logo.png")
 }, 9500)
 
 setTimeout(() => {
-  dotsManager.addDonation("Test Company I")
+  dotsManager.addDonation("Test Company I", "./imgs/logo.png")
 }, 10000)
 
 setTimeout(() => {
-  dotsManager.addDonation("Test Company J")
+  dotsManager.addDonation("Test Company J", "./imgs/logo.png")
 }, 11000)
 
 setTimeout(() => {
-  dotsManager.addDonation("Test Company K")
+  dotsManager.addDonation("Test Company K", "./imgs/logo.png")
 }, 12000)
 
 setTimeout(() => {
-  dotsManager.addDonation("Test Company L")
+  dotsManager.addDonation("Test Company L", "./imgs/logo.png")
 }, 13000)
 
 setTimeout(() => {
-  dotsManager.addDonation("Test Company M")
+  dotsManager.addDonation("Test Company M", "./imgs/logo.png")
 }, 13500)
 
 setTimeout(() => {
-  dotsManager.addDonation("Test Company N")
+  dotsManager.addDonation("Test Company N", "./imgs/logo.png")
 }, 15000)
 
 setTimeout(() => {
-  dotsManager.addDonation("Test Company O")
+  dotsManager.addDonation("Test Company O", "./imgs/logo.png")
 }, 15500)
 
 setTimeout(() => {
-  dotsManager.addDonation("Test Company P")
+  dotsManager.addDonation("Test Company P", "./imgs/logo.png")
 }, 18000)
 
 setTimeout(() => {
-  dotsManager.addDonation("Test Company Q")
+  dotsManager.addDonation("Test Company Q", "./imgs/logo.png")
 }, 20000)
 
 setTimeout(() => {
-  dotsManager.addDonation("Test Company H")
+  dotsManager.addDonation("Test Company H", "./imgs/logo.png")
 }, 23000)
 
 setTimeout(() => {
-  dotsManager.addDonation("Test Company I")
+  dotsManager.addDonation("Test Company I", "./imgs/logo.png")
 }, 25000)
 
 setTimeout(() => {
-  dotsManager.addDonation("Test Company J")
+  dotsManager.addDonation("Test Company J", "./imgs/logo.png")
 }, 28000)
